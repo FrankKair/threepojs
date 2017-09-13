c1657bc111c63dd03184b90aaa442b90f2fbdade
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-09-13 13:40:56 -0300

    Use JS only

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const Promise = require('bluebird');
-const exec = require('child_process').exec;
-const jsStringify = require('javascript-stringify');
+const xlsxToJson = require('xlsx-to-json');
 
-function promiseFrom(child) {
-  return new Promise(function(resolve, reject) {
-    child.addListener("exit", resolve);
-    child.addListener("error", reject);
-  });
-}
+const filePath = process.argv[2];
 
-const path_to_file = process.argv.slice(2, 3);
-if (path_to_file.length === 0) {
+if (!filePath) {
   console.log('No input. Provide the path to a XLSX file.');
-  process.exit();
+  return;
 }
-const childProcess = exec(`ruby xlsxtojson.rb ${path_to_file}`);
 
-promiseFrom(childProcess).then(function(result) {
-  const data = fs.readFileSync('strings.json', 'utf8');
-  const jsObject = jsStringify(JSON.parse(data), null, 2);
-  fs.writeFile("strings.js", jsObject, function(error) {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("strings.js was generated successfully!");
-    fs.unlinkSync('strings.json');
-  });
-}, function (error) {
-    console.log(`Promise rejected: ${error}`);
+xlsxToJson({ input: filePath, output: null }, (err, rows) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const stringsObject = rows.reduce((stringsPartial, row) => {
+    const { key, ...values } = row;
+    return { ...stringsPartial, [key]: values };
+  }, {});
+
+  const strings = JSON.stringify(stringsObject, null, 2);
+  fs.writeFileSync('strings.js', `${strings}\n`);
+
+  console.log('strings.js was generated successfully!');
 });
